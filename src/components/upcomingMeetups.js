e61caa4759835631f724cf1c789bd872c80dbdda
Benjamin Lannon <benjamin@lannonbr.com>
2019-01-10 22:03:18 -0500

    Continuing work on Meetups components

--- src/components/upcomingMeetups.js
+++ src/components/upcomingMeetups.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import { StaticQuery, graphql } from 'gatsby'
+import { StaticQuery, graphql, Link } from 'gatsby'
+import moment from 'moment'
 
 const query = graphql`
   {
     allMeetupEvent(
       filter: { status: { eq: "upcoming" } }
       sort: { fields: local_date }
-      limit: 5
+      limit: 3
     ) {
       edges {
         node {
@@ -14,11 +15,31 @@ const query = graphql`
           name
           description
           local_date
+          link
         }
       }
     }
   }
 `
+
+/**
+ * Parse out first paragraph of event description
+ * @param {string} desc - html string recieved from Meetup API
+ */
+function parseDescription(desc) {
+  let findStr = "What we'll do<br/>"
+
+  let idx = desc.indexOf(findStr)
+
+  if (idx === -1) {
+    findStr = '<p>'
+    idx = desc.indexOf(findStr)
+  }
+
+  let idxEnd = desc.indexOf('</p>', idx)
+
+  return desc.slice(idx + findStr.length, idxEnd)
+}
 
 function UpcomingMeetups() {
   return (
@@ -26,15 +47,44 @@ function UpcomingMeetups() {
       query={query}
       render={data => (
         <>
-          <h1>Next 5 meetups</h1>
-          {data.allMeetupEvent.edges.map(({ node }) => (
-            <div>
-              <h2>{node.name}</h2>
-              <div dangerouslySetInnerHTML={{ __html: node.description }} />
-              <time>{node.local_date}</time>
-              <hr />
-            </div>
-          ))}
+          <h2>Upcoming developer events</h2>
+          {data.allMeetupEvent.edges.map(({ node }) => {
+            let description = parseDescription(node.description)
+            let parsedDate = moment(node.local_date, 'YYYY-MM-DD').format(
+              'dddd, MMMM Do, YYYY'
+            )
+
+            return (
+              <div key={node.id}>
+                <strong>
+                  <time>{parsedDate}</time>
+                </strong>
+                <h4>
+                  <a
+                    style={{ color: '#008AFF', textDecoration: 'none' }}
+                    href={node.link}
+                  >
+                    {node.name}
+                  </a>
+                </h4>
+                <blockquote
+                  style={{
+                    borderLeft: '5px solid #eee',
+                    paddingLeft: 26,
+                    marginLeft: 0,
+                    color: '#808080',
+                    fontStyle: 'italic',
+                  }}
+                >
+                  <p>{description}</p>
+                </blockquote>
+                <p>
+                  Learn more about <Link to="#">Project</Link>
+                </p>
+                <hr />
+              </div>
+            )
+          })}
         </>
       )}
     />
